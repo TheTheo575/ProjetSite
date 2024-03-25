2487836838f3ac56111100d3f96067a9bf8e1ad6
MarcantJeremie <144245347+MarcantJeremie@users.noreply.github.com>
2024-03-25 09:35:29 +0100

    ajout

--- site-projet/site-projet/pages_php/setting.php
+++ site-projet/site-projet/pages_php/setting.php
@@ -50,5 +50,9 @@
 	try {
 		$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		catch(Exception $e){
+			die("Erreur : " . $e->getMessage());
+        }
 	}
 ?>
