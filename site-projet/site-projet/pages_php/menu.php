84d03776bed045a666e74bcede23b4b64c76dfc2
MarcantJeremie <144245347+MarcantJeremie@users.noreply.github.com>
2024-03-25 08:26:37 +0100

    enlever le coucou

--- site-projet/site-projet/pages_php/menu.php
+++ site-projet/site-projet/pages_php/menu.php
@@ -112,7 +112,7 @@
                     </div>   <!--Fin face arriere de la carte -->
                 </div>   <!--Fin d'une carte -->
                 <div class="recette">
-                    <!-- Debut d'une carte coucou -->
+                    <!-- Debut d'une carte -->
                     <div class="card-front">
                         <!--Face avant de la carte -->
                         <div>Quiche Aux Poireaux</div>
