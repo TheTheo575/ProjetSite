c10b4a615f6dc9c087962a5936bd5040746085a9
MarcantJeremie <144245347+MarcantJeremie@users.noreply.github.com>
2024-03-25 09:36:50 +0100

    catch

--- site-projet/site-projet/pages_php/setting.php
+++ site-projet/site-projet/pages_php/setting.php
@@ -50,9 +50,8 @@
 	try {
 		$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		catch(Exception $e){
-			die("Erreur : " . $e->getMessage());
-        }
+	}
+	catch(Exception $e){
+		die("Erreur : " . $e->getMessage());
 	}
 ?>
