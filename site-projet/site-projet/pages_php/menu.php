b8e05fc1b87137cb47d9d04571248b4689370859
TheoNEVEU <theo.neveu@student.junia.com>
2024-04-15 09:32:17 +0200

    modiffooter

--- site-projet/site-projet/pages_php/menu.php
+++ site-projet/site-projet/pages_php/menu.php
@@ -1,11 +1,13 @@
 <?php
     session_start();
     include_once 'setting.php';
+
+    $_SESSION["current_page"]="menu";
     
     if (!isset($_SESSION['accueil'])|| !$_SESSION['accueil']) { // Si l'utilisateur n'est pas passé par l'accueil on l'y renvoie
         header('location:accueil.php');
     }
-    $_SESSION["current_page"]="menu";
+    
 
     $reqPrep="SELECT * FROM plats WHERE type=?";//La requete SQL SELECT
     $req = $conn->prepare($reqPrep); //Préparation de la requete
