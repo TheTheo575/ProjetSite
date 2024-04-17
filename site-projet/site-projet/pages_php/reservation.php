8fb06f2b4345508e4ef581b09d4393f407b99504
TheoNEVEU <theo.neveu@student.junia.com>
2024-04-17 11:43:24 +0200

    ajout de nav et footer

--- site-projet/site-projet/pages_php/reservation.php
+++ site-projet/site-projet/pages_php/reservation.php
@@ -5,7 +5,7 @@
     if (!isset($_SESSION['accueil'])|| !$_SESSION['accueil']) { // Si l'utilisateur n'est pas passé par l'accueil on l'y renvoie
         header('location:accueil.php');
     }
-    $_SESSION["current_page"]="reservation";
+    $_SESSION["current_page"]="reserv";
 ?>
 	
 <!DOCTYPE html>
@@ -33,44 +33,9 @@
         <!--partie du Html en haut de la page avec le logo et le nom du réstaurant-->
     </header>
 
-    <nav><!--Menu de navigation-->
-    <!--Tous ce code permet d'accéder au différentes pages de notre site ( ce sont des liens)-->
     <?php
-    if(!isset($_SESSION['authentifie']) || $_SESSION['authentifie']=false || $_SESSION['admin']=false){
-        ?>
-        <div class="conteneur-nav">
-            <a href="../pages_php/accueil.php" class="lien-interne">Accueil</a>
-            <a href="../pages_php/menu.php" class="lien-interne">Menu</a>
-            <a href="../pages_php/accueil_eng.php" class="lien-interne"><b><u>FRA</u></b>/ENG</a>
-            <a href="../pages_php/reservation.php" class="lien-interne">Réservation</a>
-            <a href="../pages_php/collab.php" class="lien-interne">Partenaires</a>
-            <?php 
-        if(!isset($_SESSION['authentifie']) || $_SESSION['authentifie']=false){
-            ?>
-            <a href="connexion.php" class="lien-interne" style="fontsize: 2px;">Se connecter</a>
-            <?php
-        }
-        else{
-            ?>
-            <a href="logout.php" class="lien-interne" style="fontsize: 2px;">Se déconnecter</a>
-            <?php
-        }
-        ?>
-        </div>
-        <?php
-    }
-    elseif($_SESSION['admin']=true){
-        ?>
-        <div class="conteneur-nav">
-            <a href="../pages_php/accueil.php" class="lien-interne">Accueil</a>
-            <a href="../pages_php/menu.php" class="lien-interne">Menu</a>
-            <a href="../pages_php/reservation.php" class="lien-interne">Réservations</a>
-            <a href="logout.php" class="lien-interne" style="fontsize: 2px;"><b><u>Se déconnecter</u></b></a>
-        </div>
-        <?php
-    }
+    include "nav.php";
     ?>
-    </nav>
     
     
     
