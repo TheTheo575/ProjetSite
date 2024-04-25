e5d3752bbfe31588ab9c5432406b19ace4a4520c
TheoNEVEU <theo.neveu@student.junia.com>
2024-04-25 15:46:24 +0200

    rapport

--- site-projet/site-projet/pages_php/nav.php
+++ site-projet/site-projet/pages_php/nav.php
@@ -3,11 +3,14 @@
 <?php
     if(isset($_SESSION["admin"]) && $_SESSION["admin"]==true){
         echo'<div class="conteneur-nav">';
-        if($_SESSION["current_page"]=="accueil"){echo'<a href="../pages_php/accueil.php" class="lien-interne"><b><u>Accueil</u></b></a>';}
+        if($_SESSION["current_page"]=="accueil"){
+            echo'<a href="../pages_php/accueil.php" class="lien-interne"><b><u>Accueil</u></b></a>';}
         else{ echo'<a href="../pages_php/accueil.php" class="lien-interne">Accueil</a>';}
-        if($_SESSION["current_page"]=="menuadmin"){ echo'<a href="../pages_php/menu_admin.php" class="lien-interne"><b><u>Menu</u></b></a>';}
+        if($_SESSION["current_page"]=="menuadmin"){
+            echo'<a href="../pages_php/menu_admin.php" class="lien-interne"><b><u>Menu</u></b></a>';}
         else{ echo'<a href="../pages_php/menu_admin.php" class="lien-interne">Menu</a>';}
-        if($_SESSION["current_page"]=="reservadmin"){ echo'<a href="../pages_php/reservations_admin.php" class="lien-interne"><b><u>Réservations</u></b></a>';}
+        if($_SESSION["current_page"]=="reservadmin"){ 
+            echo'<a href="../pages_php/reservations_admin.php" class="lien-interne"><b><u>Réservations</u></b></a>';}
         else{ echo'<a href="../pages_php/reservations_admin.php" class="lien-interne">Réservations</a>';}
         echo'<a href="logout.php" class="lien-interne" style="fontsize: 2px;">Se déconnecter</a>';
         echo'admin = '.$_SESSION['admin'];
